198a3a66b9e3e1cef387074b703cf318584790f0
Luke-Crichton <18lcrichton@wakatipu.school.nz>
2022-04-12 18:20:38 +1200

    Opened repository

---
+++ main.py
@@ -0,0 +1,2 @@
+import nbateams
+
---
+++ nbateams.py
@@ -0,0 +1,113 @@
+
+
+
+def boston():
+    boston_list = ["Marcus Smart", "Jaylen Brown", "Jayson Tatum", "Al Hordford", "Robert Williams III"]
+    return boston_list
+
+def brooklyn():
+    brooklyn_list = ["Kyrie Irving", "Seth Curry", "Kevin Durant", "Ben Simmons", "Andre Drummond"]
+    return brooklyn_list
+
+def newyork():
+    newyork_list = ["Immanuel Quickley", "Quentin Grimes", "RJ Barrett", "Obi Toppin", "Mitchell Robinson"]
+    return newyork_list
+
+def philly():
+    philly_list = ["Tyrese Maxey", "James Harden", "Matisse Thybulle", "Tobias Harris", "Joel Embiid"]
+    return philly_list
+
+def toronto():
+    toronto_list = ["Fred Van Vleet", "Gary Trent Jr.", "Og Anunoby", "Scottie Barnes", "Pascal Siakam"]
+    return toronto_list
+
+def chicago():
+    chicago_list = ["Lonzo Ball", "Alex Caruso", "Zach LaVine", "DeMar DeRozan", "Nikola Vucevic"]
+    return chicago_list
+
+def cleveland():
+    cleveland_list = ["Darius Garland", "Isaac Okoro", "Caris LaVert", "Evan Mobley", "Jarrett Allen"]
+    return cleveland_list
+
+def detroit():
+    detroit_list = ["Cade Cunningham", "Hamidou Diallo", "Saddiq Bey", "Jerami Grant", "Isaiah Stewart"]
+    return detroit_list
+
+def indiana():
+    indiana_list = ["Malcom Brogdon", "Tyrese Haliburton", "Buddy Hield", "T.J. Warren", "Myles Turner"]
+    return indiana_list
+
+def milwaukee():
+    milwaukee_list = ["Jrue Holiday", "Grayson Allen", "Khris Middleton", "Giannis Antetokounmpo", "Brook Lopez"]
+    return milwaukee_list
+
+def atlanta():
+    atlanta_list = ["Trae Young", "Kevin Huerter", "De'Andre Hunter", "John Collins", "Clint Capela"]
+    return atlanta_list
+
+def charlotte():
+    charlotte_list = ["LaMelo Ball", "Terry Rozier III", "Kelly Oubre Jr.", "Miles Bridges", "Mason Plumlee"]
+    return charlotte_list
+
+def miami():
+    miami_list = ["Kyle Lowry", "Duncan Robinson", "Jimmy Butler", "P.J. Tucker", "Bam Adebayo"]
+    return miami_list
+
+def orlando():
+    orlando_list = ["Cole Anthony", "Jalen Suggs", "Franz Wagner", "Wendell Carter Jr.", "Mo Bamba"]
+    return orlando_list
+
+def washington():
+    washington_list = ["Ish Smith", "Bradley Beal", "Corey Kispert", "Kyle Kuzma", "Kristaps Porzingis"]
+    return washington_list
+
+def golden():
+    golden_list = ["Stephen Curry", "Klay Thompson", "Andrew Wiggins", "Draymond Green", "Kevon Looney"]
+    return golden_list
+
+def lac():
+    lac_list = ["Reggie Jackson", "Paul George", "Terance Mann", "Kawhi Leonard", "Ivica Zubac"]
+    return lac_list
+
+def lal():
+    lal_list = ["Russell Westbrook", "Malik Monk", "LeBron James", "Anthony Davis", "Dwight Howard"]
+    return lal_list
+
+def pheonix():
+    pheonix_list = ["Chris Paul", "Devin Booker", "Mikal Bridges", "Jae Crowder", "Deandre Ayton"]
+    return pheonix_list
+
+def sac():
+    sac_list = ["De'Aaron Fox", "Davion Mitchell", "Harrison Barnes", "Domantas Sabonis", "Richaun Holmes"]
+    return sac_list
+
+def denver():
+    denver_list = ["Jamal Murray", "Monte Morris", "Micheal Porter Jr.", "Aaron Gordon", "Nikola Jokic"]
+    return denver_list
+
+def min():
+    min_list = ["De'Angelo Russell", "Patrick Beverly", "Anthony Edwards", "Jarred Vanderbilt", "Karl-Anthony Towns"]
+    return min_list
+
+def oklahoma():
+    oklahoma_list = ["Shai Gilgeous-Alexander", "Josh Giddey", "Luguentz Dort", "Darius Bazley", "Derrick Favours"]
+    return oklahoma_list
+
+def portland():
+    portland_list = ["Damian Lillard", "Josh Hart", "Joe Ingles", "Nassir Little", "Jusuf Nurkic"]
+    return portland_list
+
+def utah():
+    utah_list = ["Mike Conley", "Donovan Mitchell", "Bojan Bogdanovic", "Royce O'Neale", "Rudy Gobert"]
+    return utah_list
+
+def dallas():
+    dallas_list = ["Luka Doncic", "Jalen Brunson", "Reggie Bullock", "Dorian Finney-Smith", "Dwight Powell"]
+    return dallas_list
+
+def houston():
+    houston_list = ["Kevin Porter Jr.", "Jalen Green", "Eric Gordon", "Alpren Sengun", "Christian Wood"]
+    return houston_list
+
+def memphis():
+    memphis_list = ["Ja Morant", "Desmond Bane", "Ziare Williams", "Jaren Jackson Jr.", "St"]
